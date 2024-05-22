09a151ddfd3778b28d8ce5f63e2069e46569db21
JinHur_Server <jinh2352@gmail.com>
2024-05-22 20:20:35 +0900

    로컬 Redis 연결 OK 원격 Redis 연결 실패, config 확인 필요

--- RedisTest/Main.cpp
+++ RedisTest/Main.cpp
@@ -3,5 +3,23 @@
 int main() {
 	RedisCpp::CRedisConn redisConn;
 
-	redisConn.connect("127.0.0.1", 6379);
+	bool connected = false; 
+
+	if (!redisConn.connect("127.0.0.1", 6379)) {
+		std::cout << "connect error " << redisConn.getErrorStr() << std::endl;
+	}
+	else {
+		connected = true;
+		UINT32 retval;
+		if (!redisConn.set("Hello", "My Name is Jin", retval)) {
+			std::cout << "set error " << redisConn.getErrorStr() << std::endl;
+		}
+		else {
+			std::cout << retval << std::endl;
+		}
+
+	}
+
+	redisConn.disConnect();
+
 }
